bf69a11b46428148ed399c8037bbf252ba20caca
Stephen Jones <srjones@consilio.com>
2016-07-28 13:34:32 -0500

    Updated readme and CSI

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 csi.js
 ======
 
-client side includes via javascript
+Client-side includes via JavaScript
 
-usage
+Usage
 ======
-
-Simply include `csi.min.js` in your `<head>` and add a `data-include` attribute on any valid HTML element. csi.js will fetch the value of that attribute, and **replace** the element with the fetched document.
+- Include `csi.min.js` in your `<head>`
+- Add a `data-include` attribute on any valid HTML element.
+  - csi.js will fetch the value of that attribute, and replace the **entire element** with the fetched document.
+  - Or, if you'd like to keep the element and replace **only its contents** with the fetched document, add a 'data-include-within="true"' attribute.
+- Call csi.loadAllIncludes() with optional callbacks for completion and progress notifications
+- Note: CSI.js requires no other libraries
 
 If, `include-me.html` looked like this:
 
@@ -18,21 +22,36 @@ Then, a document like this:
 
 ```
 <body>
+
 <div data-include="include-me.html"></div>
+<div id="keepme" data-include-within="true" data-include="include-me.html"></div>
+
+window.onload = function(){
+	csi.loadAllIncludes(
+		function(){alert('finished')}
+	);
+};
+
 </body>
 ```
 
-would end up rendering like this:
+Then, the page will load the two includes, show 'finished' in an alert box and the page will be rendered like this:
 
 ```
 <body>
 <h1>Hello, world!</h1>
+<div id="keepme" data-include-within="true" data-include="include-me.html">
+<h1>Hello, world!</h1>
+</div>
 </body>
 ```
 
-from file
+About loading from local files
 ======
 
-csi.js also works from the filesystem, enabling front end developers to splice HTML for back end implementations without the need of running a local HTTP server or copying files to a remote environment for testing to leverage server side includes.
+csi.js also works from the filesystem, enabling front end developers to splice HTML for back end implementations without the
+need of running a local HTTP server or copying files to a remote environment for testing to leverage server side includes.
 
-The only caveat is Chrome, which restricts access to local files via AJAX. To resolve this, simply add `--allow-file-access-from-files` to your Chrome runtime. All other modern browsers work on direct files without any hassle. csi.js also works fine from any web server, assuming you are following appropriate CORS policies.
+The only caveat is Chrome, which restricts access to local files via AJAX. To resolve this, simply add `--allow-file-access-
+from-files` to your Chrome command line (see [Allow Chrome File Access](https://chrome-allow-file-access-from-file.blogspot.com/)). All other modern browsers work on direct files without any hassle. csi.js also works fine
+from any web server, assuming you are following appropriate CORS policies.
--- src/csi.js
+++ src/csi.js
@@ -1,19 +1,56 @@
-window.onload = function() {
-	var elements = document.getElementsByTagName('*'),
-		i;
-	for (i in elements) {
-		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
-			fragment(elements[i], elements[i].getAttribute('data-include'));
+var csi = {
+	aIncludeElements: [],
+	nLoadedElements: 0,
+
+	// load contents of all data-include elements
+	loadAllIncludes: function(fnAllLoaded, fnProgress) {
+
+		fnAllLoaded = (typeof(fnAllLoaded) != "undefined") ? fnAllLoaded : false;
+		fnProgress = (typeof(fnProgress) != "undefined") ? fnProgress : false;
+
+		if ( fnProgress ) fnProgress( "csi: entering loadAllIncludes()..." );
+
+		var elements = document.getElementsByTagName('*');
+
+		// find all elements with data-include attribute
+		for (var iElement = 0; iElement < elements.length; ++iElement) {
+			if (elements[iElement].hasAttribute && elements[iElement].hasAttribute('data-include')) {
+				this.aIncludeElements.push(elements[iElement]);
+			}
 		}
-	}
-	function fragment(el, url) {
-		var localTest = /^(?:file):/,
+
+		if ( fnProgress ) fnProgress("csi: Loading " + this.aIncludeElements.length + " data-include elements");
+
+		// for all data-include elements, load their contents from data-include source file
+		for (var element in this.aIncludeElements) {
+			var dataIncludeURL = this.aIncludeElements[element].getAttribute('data-include');
+			var dataReplaceWithin = this.getAttribute(this.aIncludeElements[element], 'data-include-within', 'false');
+			var replaceType = (dataReplaceWithin == "true") ? "loading within" : "replacing";
+			if ( fnProgress ) fnProgress("csi: Loading include \"" + dataIncludeURL + "\", " + replaceType + "...");
+			this.loadFragment(
+				this.aIncludeElements[element],
+				dataIncludeURL,
+				dataReplaceWithin,
+				function() {
+					++csi.nLoadedElements;
+					if ( fnProgress ) fnProgress("csi: Loaded fragment #" + csi.nLoadedElements + " of " + csi.aIncludeElements.length);
+					// if this is the last include loaded, notify the caller with fnAllLoaded()
+					if ( csi.nLoadedElements == csi.aIncludeElements.length ) {
+						if ( fnAllLoaded ) fnAllLoaded();
+					}
+				}
+			);
+		}
+		if ( fnProgress ) fnProgress( "csi: exiting loadAllIncludes()..." );
+	},
+	loadFragment: function(el, url, replaceWithin, fnLoaded) {
+			var localTest = /^(?:file):/,
 			xmlhttp = new XMLHttpRequest(),
 			status = 0;
 
 		xmlhttp.onreadystatechange = function() {
 			/* if we are on a local protocol, and we have response text, we'll assume
- *  				things were sucessful */
+			 * things were sucessful */
 			if (xmlhttp.readyState == 4) {
 				status = xmlhttp.status;
 			}
@@ -21,15 +58,32 @@ window.onload = function() {
 				status = 200;
 			}
 			if (xmlhttp.readyState == 4 && status == 200) {
-				el.outerHTML = xmlhttp.responseText;
+				if ( replaceWithin == "true" ) {
+					el.innerHTML = xmlhttp.responseText;
+				}
+				else {
+					el.outerHTML = xmlhttp.responseText;
+				}
+				fnLoaded();
 			}
 		}
 
-		try { 
+		try {
 			xmlhttp.open("GET", url, true);
 			xmlhttp.send();
 		} catch(err) {
 			/* todo catch error */
 		}
+	},
+	// get element's attribute or default value if attribute isn't set
+	getAttribute: function( el, attributeName, defaultValue ) {
+		var value = defaultValue;
+		if ( el.hasAttribute(attributeName) ) {
+			value = el.getAttribute(attributeName);
+			if ( typeof(value) == "undefined" || typeof(value) == null || value == "" ) {
+					value = defaultValue;
+			}
+		};
+		return value;
 	}
-}
+};
